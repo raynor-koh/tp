ba39f37efad0d85e089b07e928e0b551c2205006
raynor-koh <64368303+raynor-koh@users.noreply.github.com>
2026-09-22 00:53:22 +0800

    feat: add parameters to RemarkCommand

--- src/main/java/seedu/address/logic/commands/RemarkCommand.java
+++ src/main/java/seedu/address/logic/commands/RemarkCommand.java
@@ -1,7 +1,9 @@
 package seedu.address.logic.commands;
 
 import static seedu.address.logic.parser.CliSyntax.PREFIX_REMARK;
+import static seedu.address.commons.util.CollectionUtil.requireAllNonNull;
 
+import seedu.address.commons.core.index.Index;
 import seedu.address.logic.commands.exceptions.CommandException;
 import seedu.address.model.Model;
 
@@ -18,9 +20,35 @@ public class RemarkCommand extends Command {
             + PREFIX_REMARK + "[REMARK]\n"
             + "Example: " + COMMAND_WORD + " 1 " + PREFIX_REMARK + "Likes to swim.";
     public static final String MESSAGE_NOT_IMPLEMENTED_YET = "Remark command not implemented yet";
+    public static final String MESSAGE_ARGUMENTS = "Index: %1$d, Remark: %2$s";
+
+    private final Index index;
+    private final String remark;
+
+    public RemarkCommand(Index index, String remark) {
+        requireAllNonNull(index, remark);
+        this.index = index;
+        this.remark = remark;
+    }
+
+    /** Creates the placeholder command used while the remark parser is not yet implemented. */
+    public RemarkCommand() {
+        this(Index.fromOneBased(1), "");
+    }
 
     @Override
     public CommandResult execute(Model model) throws CommandException {
-        throw new CommandException(MESSAGE_NOT_IMPLEMENTED_YET);
+        throw new CommandException(String.format(MESSAGE_ARGUMENTS, index.getOneBased(), remark));
+    }
+
+    @Override
+    public boolean equals(Object other) {
+        if (other == this) {
+            return true;
+        }
+        if (!(other instanceof RemarkCommand otherRemarkCommand)) {
+            return false;
+        }
+        return index.equals(otherRemarkCommand.index) && remark.equals(otherRemarkCommand.remark);
     }
 }
--- src/test/java/seedu/address/logic/commands/RemarkCommandTest.java
+++ src/test/java/seedu/address/logic/commands/RemarkCommandTest.java
@@ -1,7 +1,13 @@
 package seedu.address.logic.commands;
 
 import static seedu.address.logic.commands.CommandTestUtil.assertCommandFailure;
+import static seedu.address.logic.commands.CommandTestUtil.VALID_REMARK_AMY;
+import static seedu.address.logic.commands.CommandTestUtil.VALID_REMARK_BOB;
 import static seedu.address.testutil.TypicalPersons.getTypicalAddressBook;
+import static seedu.address.testutil.TypicalIndexes.INDEX_FIRST_PERSON;
+import static seedu.address.testutil.TypicalIndexes.INDEX_SECOND_PERSON;
+import static org.junit.jupiter.api.Assertions.assertFalse;
+import static org.junit.jupiter.api.Assertions.assertTrue;
 
 import org.junit.jupiter.api.Test;
 
@@ -14,6 +20,19 @@ public class RemarkCommandTest {
 
     @Test
     public void execute() {
-        assertCommandFailure(new RemarkCommand(), model, RemarkCommand.MESSAGE_NOT_IMPLEMENTED_YET);
+        String remark = "Some remark";
+        assertCommandFailure(new RemarkCommand(INDEX_FIRST_PERSON, remark), model,
+                String.format(RemarkCommand.MESSAGE_ARGUMENTS, INDEX_FIRST_PERSON.getOneBased(), remark));
+    }
+
+    @Test
+    public void equals() {
+        RemarkCommand standardCommand = new RemarkCommand(INDEX_FIRST_PERSON, VALID_REMARK_AMY);
+        assertTrue(standardCommand.equals(new RemarkCommand(INDEX_FIRST_PERSON, VALID_REMARK_AMY)));
+        assertTrue(standardCommand.equals(standardCommand));
+        assertFalse(standardCommand.equals(null));
+        assertFalse(standardCommand.equals(new ClearCommand()));
+        assertFalse(standardCommand.equals(new RemarkCommand(INDEX_SECOND_PERSON, VALID_REMARK_AMY)));
+        assertFalse(standardCommand.equals(new RemarkCommand(INDEX_FIRST_PERSON, VALID_REMARK_BOB)));
     }
 }
